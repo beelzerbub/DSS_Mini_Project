f021a881b856c6c37bd0a3cb8978c2fe39378a7a
Ratchakran Prapanon <beelzerbub@hotmail.com>
2017-04-19 06:05:23 +0700

    UI Phase

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Title Page</title>
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="css/bootstrap/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/assets/style.css"/>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<fieldset>
+					<legend>TEST</legend>
+					<form action="assets/service/prediction.php" class="form-inline" id="predict_form" name="predict_form" role="form" method="POST" data-toggle="validator">
+						<div class="row">
+							<div class="col-md-9">
+								<div class="col-md-6"></div>
+								<div class="col-md-6"></div>
+							</div>
+						</div>
+					</form>
+				</fieldset>
+			</div>
+		</div>
+	</div>
+	<!-- jQuery -->
+	<script src="js/jquery/jquery.min.js"></script>
+	<!-- Bootstrap JavaScript -->
+	<script src="js/bootstrap/bootstrap.min.js"></script>
+	<script src="js/bootstrap/validator.min.js"></script>
+</body>
+</html>
